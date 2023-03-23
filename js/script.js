1eb960b3688b90012711263a69efff454fd669f6
sirri1222 <hyeonji159753@gmail.com>
2023-03-23 10:00:43 +0900

    visual 진행중

---
+++ js/script.js
@@ -0,0 +1,36 @@
+window.onload = function () {
+  // 모바일 메뉴 관련
+  const navMb = document.querySelector(".nav-mb");
+  const mbWrap = document.querySelector(".mb-wrap");
+  navMb.addEventListener("click", function () {
+    // 인라인 방식
+    // if (mbWrap.style.display === "block") {
+    //   mbWrap.style.display = "none";
+    // } else {
+    //   mbWrap.style.display = "block";
+    // }
+
+    // 클래스 추가 방식
+    const state = mbWrap.classList.contains("active");
+    if (state) {
+      navMb.classList.remove("active");
+      mbWrap.classList.remove("active");
+    } else {
+      navMb.classList.add("active");
+      mbWrap.classList.add("active");
+    }
+  });
+  // 비주얼 슬라이드
+  new Swiper(".swVisual", {
+    loop: true,
+    speed: 800,
+    autoplay: {
+      delay: 1000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".sw-visual-next",
+      prevEl: ".sw-visual-prev",
+    },
+  });
+};
